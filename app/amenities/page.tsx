5836d174149a04edb4926084506ac9c3715cbf9c
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2024-07-22 23:27:19 +0530

    fix: 1 and 9 is fixed

--- app/amenities/page.tsx
+++ app/amenities/page.tsx
@@ -1,20 +1,20 @@
-"use client";
-import Header from "components/Header";
-import Footer from "components/Footer";
-import styles from "./index.module.scss";
-import WhatsApp from "components/WhatsApp";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import DownloadIcon from "assets/icons/DownIcon.svg";
-import MobileNavigation from "components/MobileNavigation";
-import { clubhouseAmenities, outdorrAmenities } from "../../constants";
-import Link from "next/link";
+'use client';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import styles from './index.module.scss';
+import WhatsApp from 'components/WhatsApp';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import DownloadIcon from 'assets/icons/DownIcon.svg';
+import MobileNavigation from 'components/MobileNavigation';
+import { clubhouseAmenities, outdorrAmenities } from '../../constants';
+import Link from 'next/link';
 
 const Amenities = () => {
   const [screenWidth, setScreenWidth] = useState<Number>(0);
   useEffect(() => {
     const updateScreenWidth = () => {
-      if (typeof window !== "undefined") {
+      if (typeof window !== 'undefined') {
         setScreenWidth(window.innerWidth);
       }
     };
@@ -23,18 +23,18 @@ const Amenities = () => {
     updateScreenWidth();
 
     // Event listener for screen width changes
-    if (typeof window !== "undefined")
-      window.addEventListener("resize", updateScreenWidth);
+    if (typeof window !== 'undefined')
+      window.addEventListener('resize', updateScreenWidth);
 
     // Clean up the event listener when the component unmounts
     return () => {
-      if (typeof window !== "undefined")
-        window.removeEventListener("resize", updateScreenWidth);
+      if (typeof window !== 'undefined')
+        window.removeEventListener('resize', updateScreenWidth);
     };
   }, []);
 
   return (
-    <div className={"page-container"}>
+    <div className={'page-container'}>
       <Header />
       <div className={styles.headingSection}>
         <div className={styles.heading}>
@@ -50,24 +50,24 @@ const Amenities = () => {
       <div className={styles.imageSection}>
         <div className={styles.btnContainer}>
           <Link
-            href="/assets/Elixir.pdf"
+            href='/assets/Elixir.pdf'
             download
-            target="_blank"
+            target='_blank'
             className={styles.downloadBtn}
           >
             <p>DOWNLOAD BROCHURE</p>
             <div
-              style={{ display: "flex", flexDirection: "column", gap: "2px" }}
+              style={{ display: 'flex', flexDirection: 'column', gap: '2px' }}
             >
               {(screenWidth as number) < 767 ? (
-                <Image src={DownloadIcon} alt="" width={9} height={4} />
+                <Image src={DownloadIcon} alt='' width={9} height={4} />
               ) : (
-                <Image src={DownloadIcon} alt="" width={11} height={6} />
+                <Image src={DownloadIcon} alt='' width={11} height={6} />
               )}
               {(screenWidth as number) < 767 ? (
-                <Image src={DownloadIcon} alt="" width={9} height={4} />
+                <Image src={DownloadIcon} alt='' width={9} height={4} />
               ) : (
-                <Image src={DownloadIcon} alt="" width={11} height={6} />
+                <Image src={DownloadIcon} alt='' width={11} height={6} />
               )}
             </div>
           </Link>
@@ -78,10 +78,10 @@ const Amenities = () => {
       <div className={styles.cardsSection}>
         <div className={styles.heading}>CLUBHOUSE AMENITIES</div>
         <div className={styles.cards}>
-          {clubhouseAmenities?.map((item) => {
+          {clubhouseAmenities?.map((item, idx) => {
             return (
-              <div className={styles.card}>
-                <Image src={item?.icon} alt="" />
+              <div className={styles.card} key={idx}>
+                <Image src={item?.icon} alt='' />
                 <div className={styles.text}>{item?.text}</div>
               </div>
             );
@@ -107,13 +107,13 @@ const Amenities = () => {
       <div className={styles.cardsSection}>
         <div className={styles.heading}>OUTDOOR AMENITIES</div>
         <div className={styles.cards}>
-          {outdorrAmenities?.map((item) => {
+          {outdorrAmenities?.map((item, idx) => {
             return (
-              <div className={styles.card}>
+              <div className={styles.card} key={idx}>
                 {(screenWidth as number) < 767 ? (
-                  <Image src={item?.icon} alt="" height={68} />
+                  <Image src={item?.icon} alt='' height={68} />
                 ) : (
-                  <Image src={item?.icon} alt="" />
+                  <Image src={item?.icon} alt='' />
                 )}
 
                 <div className={styles.text}>{item?.text}</div>
